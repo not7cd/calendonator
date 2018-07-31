f2cfa0cea0ee6d6938b55a6c4f6c1acf203006bc
Norbert Szulc <norbert@not7cd.net>
2018-07-31 22:37:09 +0200

    Cookiecutter

---
+++ tests/test_calendonator.py
@@ -0,0 +1,17 @@
+import unittest
+
+import calendonator
+
+
+class CalendonatorTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = calendonator.app.test_client()
+
+    def test_index(self):
+        rv = self.app.get('/')
+        self.assertIn('Welcome to calendonator', rv.data.decode())
+
+
+if __name__ == '__main__':
+    unittest.main()

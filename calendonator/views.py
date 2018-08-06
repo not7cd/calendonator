40ab80a888e86ed9bc4ee6a02902d865a6bf18e4
Norbert Szulc <norbert@not7cd.net>
2018-08-06 21:25:42 +0200

    Update lock

--- calendonator/views.py
+++ calendonator/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template, json
 
 from calendonator import app
 from calendonator.calendars import *
 import arrow
+from glom import glom
 
 
 def get_calendar():
@@ -21,3 +22,22 @@ def index():
 @app.route("/upcoming.ical")
 def aggregated_calendar():
     return str(get_calendar())
+
+def events_list(events):
+    for event in events:
+        yield glom(event, {
+            "name": "name",
+            "begin": ("begin", lambda b: b.format('YYYY-MM-DD HH:mm:ss'))
+            })
+
+@app.route("/api/now")
+def now():
+    timeline = ics.timeline.Timeline(get_calendar())
+    return json.dumps(list(events_list(timeline.now())))
+
+@app.route("/api/upcoming")
+def upcoming():
+    timeline = ics.timeline.Timeline(get_calendar())
+    now = arrow.now()
+    return json.dumps(list(events_list(timeline.start_after(now))))
+
